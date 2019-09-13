d04745086ee8381878d6e1f2f0c5890801a2164a
zhangcs <zhangchensong@gmail.com>
2019-09-13 18:11:38 +0800

    Fix Google Test environment

--- utest/unitVEC.cxx
+++ utest/unitVEC.cxx
@@ -1,5 +1,5 @@
-/** \file testGoogleTest.cxx
- *  \brief Test GTest unit-test suite
+/** \file unitVEC.cxx
+ *  \brief Unit test for VEC class implementations
  *
  *-----------------------------------------------------------------------------------
  *  Copyright (C) 2019--present by the FASP++ team. All rights reserved.
@@ -8,7 +8,28 @@
  */
 
 #include <gtest/gtest.h>
-#include "gtest_examples.cxx"
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+#include "vec.hxx"
+
+VEC v0;
+VEC v1(4,2.312);
+
+int g = 0;
+
+std::vector<double> v2{1.23, 0.98, 234.67, 456};
+
+/*---------------------------------*/
+/*--     Beginning of TEST       --*/
+/*---------------------------------*/
+
+TEST(VEC_size, Size)
+{
+    EXPECT_EQ(v0.GetSize(), 0);
+    EXPECT_EQ(v1.GetSize(), 4);
+}
 
 /*---------------------------------*/
 /*--     Beginning of main       --*/
