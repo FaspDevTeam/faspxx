aac265fabafa09855dc501c2a81e11b169df4d13
zhangcs <zhangchensong@gmail.com>
2019-09-29 14:33:58 +0800

    Add doxygen setting

---
+++ src/doxygen.hxx
@@ -0,0 +1,78 @@
+/*! \file doxygen.hxx
+ *  \brief Main page for Doygen documentation
+ *  \author Chensong Zhang
+ *  \date Sep/29/2019
+ *
+ *-----------------------------------------------------------------------------------
+ *  Copyright (C) 2019--present by the FASP++ team. All rights reserved.
+ *  Released under the terms of the GNU Lesser General Public License 3.0 or later.
+ *-----------------------------------------------------------------------------------
+ */
+ 
+/** \mainpage Introduction
+ *
+ * This software distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Lesser General Public License for more details.
+ *
+ * The FASPxx is a C++ package designed for developing parallel iterative solvers
+and preconditioners for PDEs and systems of PDEs. The main components of the
+package are standard Krylov methods, algebraic multigrid methods, geometric
+multigrid methods, Schwarz methods, and incomplete factorization methods.
+
+ */
+ 
+/**
+ * \page download How to obtain FASP++
+ *
+ * TBA
+ */
+
+/**
+ * \page build Building and Installation
+ *
+ * This is a simple instruction on building and testing. There is a top level cmake
+ * for configuration and building of the FASPxx shared library and the test programs
+ * suite. You can use a cmake-style way to compile the package; see https://cmake.org
+ * on how to use cmake for your own operating system. To compile, you alos need a C++
+ * compiler.
+ *
+ * > $ mkdir Debug; cd Debug; cmake ..
+ * > $ make
+ *
+ */ 
+
+
+/**
+ * \page developers Developers
+ *
+ * Project coordinator:
+ *
+ * - Zhang, Chensong (AMSS, Chinese Academy of Sciences, China)
+ *
+ * Current active developers (in alphabetic order):
+ *
+ * - Fan, Ronghong (AMSS, Chinese Academy of Sciences, China)
+ *
+ * - Zhang, Kailei (AMSS, Chinese Academy of Sciences, China)
+ *
+ */
+   
+/**
+ * \page doxygen_comment Doxygen
+ *
+ * We use Doxygen as our automatically documentation generator which will make our 
+ * future maintainance minimized. You can obtain the software (Windows, Linux and 
+ * OS X) as well as its manual on the official website 
+ * 
+ * http://www.doxygen.org
+ *
+ * For an ordinary user, Doxygen is completely trivial to use. We only need to use 
+ * some special marker in the usual comment as we put in c-files. 
+ *
+ */
+ 
+/*---------------------------------*/
+/*--        End of File          --*/
+/*---------------------------------*/
+ 
