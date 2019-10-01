126ed157da9c05ca74f9c48e2a6a3be358e50f9a
zkl-tech <1393756017@qq.com>
2019-10-01 19:50:21 +0800

    PCG.*

--- src/PCG.cxx
+++ src/PCG.cxx
@@ -3,54 +3,108 @@
  */
 
 #include "PCG.hxx"
-#include "MATUtil.hxx"
 #include "VECUtil.hxx"
 
-FaspRetCode CG(const MAT &A, const VEC &b, VEC &x) {
+FaspRetCode PCG::SetUp(const MAT &A_, const VEC &b_, const DBL &rtol_, const INT
+&maxIt_) {
+    if ( A_.GetNNZ() == 0 || b_.GetSize() == 0 || A_.GetRowSize() != b_.GetSize()){
+        return FaspRetCode::ERROR_INPUT_PAR;
+    }
 
-    //some basic examination
-    if ( A.GetRowSize() != A.GetColSize())
-        return FaspRetCode::ERROR_MAT_SIZE;
+    try {
+        this->A = A_;
+        this->b = b_;
+    } catch ( std::bad_alloc &ex ) {
+        throw (FaspBadAlloc(__FILE__, __FUNCTION__, __LINE__));
+    }
+    this->rtol = rtol_;
+    this->maxIt = maxIt_;
 
-    if ( CheckMATVECSize(A, x) != FaspRetCode::SUCCESS )
-        return FaspRetCode::ERROR_NONMATCH_SIZE;
+    return FaspRetCode::SUCCESS;
+}
+
+FaspRetCode PCG::SetUpPCD(const MAT &P_) {
+    if ( P_.GetColSize() != this->A.GetRowSize())
+        return FaspRetCode::ERROR_INPUT_PAR;
+
+    try {
+        this->P = P_;
+        this->pflag = 1;
+    } catch ( std::bad_alloc &ex ) {
+        throw (FaspBadAlloc(__FILE__, __FUNCTION__, __LINE__));
+    }
+
+    return FaspRetCode::SUCCESS;
+}
+
+FaspRetCode PCG::Start(VEC &x, INT &iter) {
 
     INT count = 0;
     DBL norm_prev, normk;
     DBL alpha, beta;
     VEC rk, pk, tmp;
 
-    DBL R_TOL = 1e-4;
+    try {
+        if ( x.GetSize() != A.GetColSize()) {
+            FaspRetCode retCode = FaspRetCode::ERROR_NONMATCH_SIZE;
+            throw (FaspRunTime(retCode, __FILE__, __FUNCTION__, __LINE__));
+        }
+    } catch ( FaspRunTime &ex ) {
+        return FaspRetCode::ERROR_NONMATCH_SIZE;
+    }
 
     //! r_{0} = b - A * x_{0}
-    rk = A.MultVec(x);
-
-    if ( CheckVECSize(rk, b) != FaspRetCode::SUCCESS )
-        return FaspRetCode::ERROR_NONMATCH_SIZE;
+    rk = this->A.MultVec(x);
 
     rk.Add(-1.0, 1.0, b);
 
-    pk = rk;
+    if ( pflag == 1 ) {
+        /* z0=M^{-1} * r0 */
+        /* p0=z0 */
+        /* normk=r_{j} * z_{j} */
+    } else {
+        pk = rk;
+        normk = rk.Dot(rk);
+    }
 
-    normk = rk.Dot(rk);
-
-    while ( count < A.GetRowSize() && pk.Norm2() > R_TOL ) {
-
-        tmp = A.MultVec(pk);
-        alpha = (normk / tmp.Dot(pk));
+    while ( count < maxIt && rk.Norm2() > rtol ) {
+        if ( pflag == 1 ) {
+            /* alpha = (r_{j},z_{j}) / (Ap_{j},p_{j}) */
+        } else {
+            tmp = A.MultVec(pk);
+            alpha = (normk / tmp.Dot(pk));
+        }
 
         x.Add(1.0, alpha, pk);
 
-        rk.Add(1.0, -1.0 * alpha, tmp);
-        norm_prev = normk;
-        normk = rk.Dot(rk);
-        beta = normk / norm_prev;
+        rk.Add(1.0, -alpha, tmp);
 
-        pk.Add(beta, 1.0, rk);
+        if ( pflag == 1 ) {
+            /* z_{j+1} = M^{-1} r_{j+1} */
+            /* beta_{j} = (r_{j+1},z_{j+1}) / (r_{j},z_{j}) */
+            /* p_{j+1} = z_{j+1} + beta_{j} p_{j} */
+        } else {
+            norm_prev = normk;
+            normk = rk.Dot(rk);
+            beta = normk / norm_prev;
+            pk.Add(beta, 1.0, rk);
+        }
 
         count++;
-
     }
+    iter = count;
 
     return FaspRetCode::SUCCESS;
+}
+
+void PCG::CleanPCD() {
+    MAT Pre;
+    this->P = Pre;
+}
+
+void PCG::Clean() {
+    MAT Atmp;
+    this->A = Atmp;
+    VEC v;
+    this->b = v;
 }
--- src/PCG.hxx
+++ src/PCG.hxx
@@ -7,6 +7,32 @@
 
 #include "MAT.hxx"
 
-FaspRetCode CG(const MAT &A,const VEC &b,VEC &x);
+class PCG {
+private:
+    DBL rtol;
+    INT maxIt;
+    INT pflag;
+    MAT A;
+    MAT P;
+    VEC b;
+
+public:
+    PCG() : rtol(0), maxIt(0), pflag(0) {};
+
+    PCG(DBL &rtol_, DBL &maxIt_, INT &pflag_) : rtol(rtol_), maxIt(maxIt),
+                                             pflag(pflag_) {};
+
+    FaspRetCode SetUp(const MAT &A_, const VEC &b_, const DBL &rtol_, const INT
+    &maxIt_);
+
+    FaspRetCode SetUpPCD(const MAT &P);
+
+    FaspRetCode Start(VEC &x, INT &MaxIter);
+
+    void CleanPCD();
+
+    void Clean();
+
+};
 
 #endif //SRC_PCG_HXX
