ebe81c7c9287fdfcdc3367624ad6d390c9c21d65
zkl-tech <1393756017@qq.com>
2019-10-20 22:48:26 +0800

    ReadCommand.hxx

--- src/ReadCommand.hxx
+++ src/ReadCommand.hxx
@@ -7,17 +7,18 @@
 
 #include "RetCode.hxx"
 #include "MAT.hxx"
+#include "Param.hxx"
 
-
-/// read rhs or inital solution
-static FaspRetCode ReadRhs(char *filename, VEC &rhs);
-
+/*
 /// convert CSR format to CSRx matrix
 FaspRetCode ReadCSRToMAT(char *filename, MAT &mat);
-
+*/
 /// read command line parameters to achieve coefficient matrix, right hand side, and
 /// initial solution
 FaspRetCode ReadCommand(unsigned int argc, char *args[], MAT &mat, VEC &rhs, VEC
 &initGuess);
+
+// read parameters from command lines
+void ReadParam(int argc,char *args[],InitParam &init);
 
 #endif //SRC_READCOMMAND_HXX
