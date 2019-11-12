75b7a10ac8dd33512b758ffe1ff833978a681f0e
zkl-tech <1393756017@qq.com>
2019-11-12 20:15:36 +0800

    SOL.hxx

--- src/SOL.hxx
+++ src/SOL.hxx
@@ -1,8 +1,41 @@
-//
-// Created by kailei on 2019/11/11.
-//
+/**
+ * SOL header file
+ */
 
-#ifndef FASPXX_SOL_HXX
-#define FASPXX_SOL_HXX
+#ifndef SOL_HXX
+#define SOL_HXX
 
-#endif //FASPXX_SOL_HXX
+#include "Param.hxx"
+#include "VEC.hxx"
+#include "LOP.hxx"
+#include "RetCode.hxx"
+
+class SOL{
+protected:
+    // preconditioner
+    LOP *lop;
+
+public:
+    // default constructor
+    SOL():lop(nullptr){};
+
+    /// check and allocate memory
+    virtual FaspRetCode Setup(const LOP& A,const VEC& b,VEC& x,const IterParam& param) = 0;
+
+    /// build preconditioner operator
+    virtual void SetupPCD(const LOP* lop) = 0;
+
+    /// solve by SOL
+    virtual FaspRetCode Solve(const LOP& A, const VEC& b, VEC& x,IterParam& param) = 0;
+
+    /// clean preconditioner operator
+    virtual void CleanPCD() = 0;
+
+    /// Release temporary memory
+    virtual void Clean() = 0;
+
+    // destructor
+    ~SOL(){};
+};
+
+#endif // SOL_HXX
