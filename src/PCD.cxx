854f33ef9317eadb6a88c246090667c40c5956dd
zkl-tech <1393756017@qq.com>
2019-11-12 20:12:59 +0800

    PCD.*

--- src/PCD.cxx
+++ src/PCD.cxx
@@ -1,4 +1,30 @@
-//
-// Created by kailei on 2019/11/12.
-//
+/**
+ * source codes for some preconditioner
+ */
 
+#include "PCD.hxx"
+
+// overload = operator
+IdentityLOP& IdentityLOP::operator=(const IdentityLOP& lop) {
+    this->nrow = lop.nrow;
+    this->ncol = lop.ncol;
+    return *this;
+}
+
+// 'y' = operator applys 'x'
+void IdentityLOP::Apply(const VEC& x, VEC& y) const {
+    y = x;
+}
+
+// constructor
+Jacobi::Jacobi(const std::vector<double>& diag) {
+    this->diag.SetValues(diag);
+    this->nrow = diag.size();
+    this->ncol = diag.size();
+}
+
+// 'y' = operator applys 'x'
+void Jacobi::Apply(const VEC& x, VEC& y) const {
+    y = x;
+    y.PointwiseDivide(diag);
+}
--- src/PCD.hxx
+++ src/PCD.hxx
@@ -1,8 +1,36 @@
-//
-// Created by kailei on 2019/11/12.
-//
+/**
+ * a header file for some preconditioners
+ */
 
-#ifndef FASPXX_PCD_HXX
-#define FASPXX_PCD_HXX
+#ifndef PCD_HXX
+#define PCD_HXX
 
-#endif //FASPXX_PCD_HXX
+#include "LOP.hxx"
+
+// Identity linear operator
+class IdentityLOP : public LOP {
+public:
+    // constructor
+    explicit IdentityLOP(INT n) : LOP(n, n) {}
+
+    // overload = operator
+    IdentityLOP &operator=(const IdentityLOP& lop);
+
+    // 'y' = operator applys 'x'
+    virtual void Apply(const VEC& x, VEC& y) const;
+};
+
+class Jacobi:public LOP {
+private:
+    // diagonal elements
+    VEC diag;
+
+public:
+    // constructor
+    Jacobi(const std::vector<double>& diag);
+
+    // 'y' = operator applys 'x'
+    void Apply(const VEC& x, VEC& y) const;
+};
+
+#endif // PCD_HXX
