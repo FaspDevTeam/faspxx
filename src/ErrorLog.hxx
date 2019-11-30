9da7bea40426a9d327e291f18cef6ba7557b3b48
zhangcs <zhangchensong@gmail.com>
2019-11-30 22:45:51 +0800

    Fix problem with including ErrorLog.hxx

--- src/ErrorLog.hxx
+++ src/ErrorLog.hxx
@@ -14,6 +14,7 @@
 
 #include <sstream>
 #include <iomanip>
+#include <iostream>
 
 /// Print out location at (file, line) and function name
 #define _FASPXX_LOCATION_                                         \
