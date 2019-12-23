337aca153e9e4bc26f1177aca1b7ef527f3ef3e2
zhangcs <zhangchensong@gmail.com>
2019-12-23 15:17:28 +0800

    Clean up testKrylov example

--- example/testKrylov.cxx
+++ example/testKrylov.cxx
@@ -20,106 +20,102 @@
 #include "LOP.hxx"
 #include "Iter.hxx"
 
-int main(int argc, char *args[]) {
-    std::string mat_file = "../data/fdm_10X10.csr";
-    std::string vec_file = "";
-    std::string initial_guess = "";
+int main(int argc, char *args[])
+{
+    FaspRetCode retCode = FaspRetCode::SUCCESS; // Return success if no-throw
+
+    // User default parameters
     Output verbose = PRINT_NONE;
-    std::string opts = "../data/multiple_sol.opts";
-    std::string prefix = "-solver1";
-    std::string Krylov="CG";
-    double resrel = 1e-6, resabs = 1e-8;
-    int restart = 20, maxIter = 100, minIter = 0;
+    std::string parFile = "../data/multiple_sol.param";
+    std::string matFile = "../data/fdm_10X10.csr";
+    std::string rhsFile = "";
+    std::string x0File  = "";
+    std::string solAlgm = "CG";
+    double resRel = 1e-6, resAbs = 1e-8;
+    int maxIter = 100, minIter = 0, restart = 20;
+    Identity pc; // no preconditioner
 
+    // Command-line parameters
     Parameters params(argc, args);
-    params.AddParam("-sol","Krylov subspace method (CG or BiCGStab)",&Krylov);
-    params.AddParam("-mat", "Left hand side of linear system", &mat_file);
-    params.AddParam("-vec", "Right hand side of linear system", &vec_file);
-    params.AddParam("-initial", "Initial guess of solution", &initial_guess);
-    params.AddParam("-verbose", "Verbose level", &verbose);
-    params.AddParam("-opts_file", "Solver parameter file", &opts);
-
-    params.AddParam("-pref", "solver parameter prefix in file", &prefix);
-    params.AddParam("-resrel", "Relative residual tolerance", &resrel);
-    params.AddParam("-resabs", "Absolute residual tolerance", &resabs);
-    params.AddParam("-restart", "Iteration restart number", &restart);
+    params.AddParam("-par", "Solver parameter file", &parFile);
+    params.AddParam("-mat", "Left hand side of linear system", &matFile);
+    params.AddParam("-rhs", "Right hand side of linear system", &rhsFile);
+    params.AddParam("-x0", "Initial guess of solution", &x0File);
+    params.AddParam("-solAlgm", "Iterative solver type", &solAlgm);
     params.AddParam("-maxIter", "Maximum iteration steps", &maxIter);
     params.AddParam("-minIter", "Minimum iteration steps", &minIter);
+    params.AddParam("-restart", "Iteration restart number", &restart);
+    params.AddParam("-resRel", "Relative residual tolerance", &resRel);
+    params.AddParam("-resAbs", "Absolute residual tolerance", &resAbs);
+    params.AddParam("-verbose", "Verbose level", &verbose);
     params.Parse();
+
     params.Print();
 
-    FaspRetCode retCode = FaspRetCode::SUCCESS; // Return success if no-throw
+    // Initiate timer
     GetWallTime timer;
     timer.Start();
 
     // Read matrix data file
     MAT mat;
-    if ( (retCode = ReadMat(mat_file.c_str(), mat)) < 0 ) return retCode;
+    if ( (retCode = ReadMat(matFile.c_str(), mat)) < 0 ) return retCode;
     const INT nrow = mat.GetRowSize();
     const INT mcol = mat.GetColSize();
-    std::cout<<"nrow : "<<nrow<<std::endl;
-    std::cout<<"mcol : "<<mcol<<std::endl;
-    // Read or generate right-hand side
+    std::cout << "nrow: " << nrow << ", mcol: " << mcol << std::endl;
+
+    // Read the right-hand side b; if not specified, use b = 0
     VEC b;
-    if ( strcmp(vec_file.c_str(), "") != 0 )
-        ReadVEC(vec_file.c_str(), b);
+    if ( strcmp(rhsFile.c_str(), "") != 0 )
+        ReadVEC(rhsFile.c_str(), b);
     else
         b.SetValues(nrow, 0.0);
 
-    // Read or generate initial guess
+    // Read the initial guess x0; if not specified, use x0 = 0
     VEC x;
-    if ( strcmp(initial_guess.c_str(), "") != 0 )
-        ReadVEC(initial_guess.c_str(), x);
+    if ( strcmp(x0File.c_str(), "") != 0 )
+        ReadVEC(x0File.c_str(), x);
     else
         x.SetValues(mcol, 1.0);
 
-    VEC y(mcol);
     // Print problem size information
     std::cout << "Reading Ax = b costs " << timer.Stop() << "ms" << std::endl;
 
     // Change all letters of a string to lowercase
-    for(int j=0;j<Krylov.length();++j){
-        if(Krylov[j]>='A' && Krylov[j]<='Z')
-            Krylov[j]=Krylov[j]-'A'+'a';
-    }
+    for ( char & c : solAlgm ) c = tolower(c);
 
-    // Setup preconditioner
-    Identity pc;
-
-    if(Krylov=="cg"){
-        std::cout<<"CG method : "<<std::endl;
+    if ( solAlgm == "cg" ) {
+        // Setup PCG method
         CG solver;
-
-        // Setup Krylov method
         solver.SetOutput(verbose);
         solver.SetMaxIter(maxIter);
         solver.SetMinIter(minIter);
-        solver.SetRelTol(resrel);
-        solver.SetAbsTol(resabs);
         solver.SetRestart(restart);
+        solver.SetRelTol(resRel);
+        solver.SetAbsTol(resAbs);
         solver.SetPC(&pc);
         solver.Setup(mat);
+        solver.PrintParam();
 
         // PCG solve
         timer.Start();
         retCode = solver.Solve(b, x);
         std::cout << "Solving linear system costs " << std::fixed
                   << std::setprecision(2) << timer.Stop() << "ms" << std::endl;
-    }else if(Krylov=="bicgstab"){
-        std::cout<<"BiCGStab method : "<<std::endl;
+    }
+    else if ( solAlgm == "bicgstab" ) {
+        // Setup BiCGStab method
         BiCGStab solver;
-
-        // Setup Krylov method
         solver.SetOutput(verbose);
         solver.SetMaxIter(maxIter);
         solver.SetMinIter(minIter);
-        solver.SetRelTol(resrel);
-        solver.SetAbsTol(resabs);
         solver.SetRestart(restart);
+        solver.SetRelTol(resRel);
+        solver.SetAbsTol(resAbs);
         solver.SetPC(&pc);
         solver.Setup(mat);
+        solver.PrintParam();
 
-        // PCG solve
+        // BiCGStab solve
         timer.Start();
         retCode = solver.Solve(b, x);
         std::cout << "Solving linear system costs " << std::fixed
