8d357b5c4631aba83cd33a5e3b45b61f5c97b09e
zkl-tech <1393756017@qq.com>
2020-04-02 17:00:59 +0800

    unfinished GMRES

--- src/GMRES.cxx
+++ src/GMRES.cxx
@@ -1,6 +1,10 @@
 
 #include "GMRES.hxx"
 
+INT GMRES::Locate(INT row, INT col) {
+    return 0;
+}
+
 /// Setup the LGMRES method.
 FaspRetCode LGMRES::Setup(const LOP& A){
     // Set solver type
@@ -27,4 +31,72 @@ FaspRetCode LGMRES::Setup(const LOP& A){
     if ( params.verbose > PRINT_MIN ) PrintParam(std::cout);
 
     return FaspRetCode::SUCCESS;
+}
+
+/// Solve Ax=b using the LGMRES method.
+FaspRetCode LGMRES::Solve(const VEC& b, VEC& x){
+    // Local variables
+    const INT len = b.GetSize();
+    const int maxStag = MAX_STAG_NUM; // max number of stagnation checks
+    const double solStagTol = 1e-4 * params.relTol; // solution stagnation tolerance
+    const double solZeroTol = CLOSE_ZERO; // solution close to zero tolerance
+
+    int stagStep = 0, moreStep = 0;
+    double resAbs = 1.0, resRel = 1.0, denAbs = 1.0, ratio = 0.0, resAbsOld = 1.0;
+    double alpha, beta, tmpa, tmpb;
+
+    PrintHead();
+
+    // Initialize iterative method
+    numIter = 0;
+    A->Apply(x, tmp); // A * x -> rk
+    tmp.XPAY(-1.0, b); // b - rk -> rk
+
+    resAbs=tmp.Norm2();
+    if(resAbs<this->params.absTol)
+        return FaspRetCode ::SUCCESS;
+
+    while(1){
+
+        pc->Solve(tmp,r0);
+
+        beta=r0.Norm2();
+        vk=r0;
+        vk.Scale(1/beta);
+        V.Set(0,vk);
+        for(INT j=0;j<this->params.restart;++j){
+            A->Apply(vk,tmp);
+            pc->Solve(tmp,wk);
+            for(INT i=0;i<j;++i)
+                H[Locate(i,j)]=vk.Dot(wk);
+            for(INT k=0;k<j;++k){
+                V.Get(k,vk);
+                vk.Scale(H[Locate(k,j)]);
+                wk-=vk;
+            }
+            H[Locate(j+1,j)]=wk.Norm2();
+            if(H[Locate(j+1,j)]<CLOSE_ZERO)
+                break;
+            wk.Scale(1/H[Locate(j+1,j)]);
+            vk=wk;
+            V.Set(j+1,vk);
+        }
+        HouseHolder(H,betae1);
+        SolveMin(H,betae1,qk);
+        pk=x;
+        for(INT j=0;j<this->params.restart;++j){
+            V.Get(j,vk);
+            vk.Scale(qk[j]);
+            pk+=vk;
+        }
+
+        A->Apply(pk, tmp); // A * x -> rk
+        tmp.XPAY(-1.0, b); // b - rk -> rk
+
+        resAbs=tmp.Norm2();
+        if(resAbs<this->params.absTol){
+            x=pk;
+            break;
+        }
+    }
 }
--- src/GMRES.hxx
+++ src/GMRES.hxx
@@ -8,14 +8,16 @@
 #include "SOL.hxx"
 #include "VecCluster.hxx"
 
-class GMRES{
-    friend class LGMRES;
-    friend class RGMRES;
-private:
+class GMRES:public SOL{
+public:
+    INT Locate(INT row, INT col);
+
+    void HouseHolder(std::vector<DBL> H,VEC betae1);
+
     void SolveMin(std::vector<DBL> H,VEC betae1,VEC qk);
 };
 
-class LGMRES: public SOL{
+class LGMRES: public GMRES{
 private:
     VEC p0;
     VEC pk;
@@ -25,6 +27,7 @@ private:
     VEC vk;
     VEC wk;
     VEC betae1;
+    VEC qk;
     std::vector<DBL> H;
 public:
     // default construction
@@ -41,6 +44,35 @@ public:
     void Clean() override;
 
     /// Solve Ax=b using the LGMRES method.
+    FaspRetCode Solve(const VEC& b, VEC& x) override;
+};
+
+class RGMRES: public GMRES{
+private:
+    VEC p0;
+    VEC pk;
+    VEC r0;
+    VEC tmp;
+    VecCluster V;
+    VEC vk;
+    VEC wk;
+    VEC betae1;
+    std::vector<DBL> H;
+public:
+    // default construction
+    RGMRES():p0(0),pk(0),r0(0),tmp(0),V(0,0),
+             vk(0),wk(0),betae1(0),H(0){};
+
+    // destruction
+    ~RGMRES(){};
+
+    /// Setup the RGMRES method.
+    FaspRetCode Setup(const LOP& A) override;
+
+    /// Clean up RGMRES data allocated during Setup.
+    void Clean() override;
+
+    /// Solve Ax=b using the RGMRES method.
     FaspRetCode Solve(const VEC& b, VEC& x) override;
 };
 #endif //GMRES_HXX
