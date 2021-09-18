58e4916fd1e1ffd603f6724d6ae2c669b4e28a93
zhangcs <zhangchensong@gmail.com>
2021-09-18 10:21:12 +0800

    Update LGPL license

--- src/Doxygen.hxx
+++ src/Doxygen.hxx
@@ -21,7 +21,8 @@
  *
  * This software distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details:
+ * https://www.gnu.org/licenses/lgpl-3.0.html
  *
  */
 
